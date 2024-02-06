0bbdf4f3de1528ab0e7cbd125c1f0be5cfbddc1b
tmckenna19 <tdlmckenna@gmail.com>
2024-02-06 17:47:58 -0500

    calculator front finished. start logic

---
+++ script.js
@@ -0,0 +1,14 @@
+//selecting elements
+const numOne = [];
+const numTwo = [];
+// const logic 
+
+const seven = document.querySelector('.number-7')
+
+
+seven.addEventListener('click', function () {
+    console.log("7")
+})
+
+
+
