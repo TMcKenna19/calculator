d70b2222f76c8b5fbef51bed9c62192ea9227086
tmckenna19 <tdlmckenna@gmail.com>
2024-02-16 15:59:10 -0500

    basic functions started

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-"use strict";
+// Calculator Logic
 
-function displayNumber(e) {
-    console.log(e)
+
+const display = document.getElementById("display");
+
+// appendDisplay
+function appendDisplay(input) {
+    display.value += input;
 }
 
+// calculate
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+}
+
+// clearDisplay 
+function clearDisplay() {
+    display.value = "";
+}
 
 
 
